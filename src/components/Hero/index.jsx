68cc58db4eedb1c9500b1ae2ecfb66e41d66b3b0
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-16 01:08:43 +0200

    style: hero MVP version

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import img_hero from 'assets/images/background-retrowave-web.jpg';
+import img_hackerman from 'assets/images/hackerman.png';
+import img_logo from 'assets/images/ronincode-logo-20210610-3.png';
 
 const Hero = () => (
   <div className="hero">
-    <img src={img_hero} alt="Paysage retrowave 80's" />
+    <img src={img_hero} alt="Paysage retrowave 80's" className="hero-background"/>
+    <img src={img_hackerman} alt="Paysage retrowave 80's" className="hero-2"/>
+    <img src={img_logo} alt="Paysage retrowave 80's" className="hero-3"/>
+    <h2 className="hero-txt">"Ne Code<br/>plus seul !"</h2>
   </div>
 );
-
 
 export default Hero;
