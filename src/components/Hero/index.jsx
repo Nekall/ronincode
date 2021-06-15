ce9d39a9dce0d28e5ce6b355c5f727ebb2c0861f
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-15 14:12:12 +0200

    style: add Hero components

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import img_hero from 'assets/images/background-retrowave-web.jpg';
+
+const Hero = () => (
+  <div className="hero">
+    <img src={img_hero} alt="Paysage retrowave 80's" />
+  </div>
+);
+
+
+export default Hero;
