4645d7040395897742945c007c936754f4842a4f
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-16 16:23:55 +0200

    style: Profile page in progress

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Hero from "components/Hero";
+// import Hero from "components/Hero";
+import Profile from "components/Profile";
 
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
-    </div>
+    <>
+      {/* <Hero /> */}
+      <Profile />
+    </>
   )
 
 }
