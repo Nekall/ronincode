c0b93e62f54fdfc50ae5dd1d462525780aa06d67
MatteoTHP <wow-guerrier@hotmail.fr>
2021-06-16 12:40:01 +0200

    added basic react for pages article and message

---
+++ src/pages/Message/index.jsx
@@ -0,0 +1,39 @@
+const Message = () => {
+
+
+
+  const [Messages, setMessages] = useState([])
+
+
+  useEffect(() => {
+
+    fetch(`http://localhost:3000/Messages`,{
+      method:'GET',
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setMessages(data);
+
+      })
+    .catch(err => console.error(err));
+
+  }, [])
+
+  return(
+    <div>
+      <h1> Messages </h1>
+      <div>{Messages.map((message) =>
+        <div>
+          <p key={message.id}> Body: {message.body} </p>
+         
+          
+        </div>
+      
+      )}
+      </div>
+    </div>
+  )
+
+}
+
+export default Message;
