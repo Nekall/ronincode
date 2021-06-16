dcc8204ddf3649c7051bf2f6a1a0832803e14bb4
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-16 15:21:57 +0200

    fix: error confilct with old version of name component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Hero from "components/Hero";
-import CardGroupArticle from "components/CardGroupArticle";
 
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <CardGroupArticle />
     </div>
   )
 
