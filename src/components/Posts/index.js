d973e9aa68e71d9d4c1112003472f0b32a124c5b
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-16 11:49:28 +0200

    fix style: rename article to post

---
+++ src/components/Posts/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroMini from 'components/HeroMini';
+import CardGroupPost from 'components/CardGroupPost';
+
+const Posts = () => {
+  return (
+    <>
+      <HeroMini />
+      <CardGroupPost />
+    </>
+  );
+};
+
+export default Posts;
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import Navbar from "components/Navbar";
 import Hero from "components/Hero";
-import CardGroupArticle from "components/CardGroupArticle";
+import HeroMini from "components/HeroMini";
+import CardGroupPost from "components/CardGroupPost";
 import Footer from "components/Footer";
 import "style/main.scss";
 import { store } from 'reduxx/store';
@@ -15,8 +16,9 @@ const App = () => (
   <>
     <Provider store={store}>
       <Navbar />
-      <Hero />
-      <CardGroupArticle />
+      {/* <Hero /> */}
+      <HeroMini />
+      <CardGroupPost />
       <Footer />
     </Provider>
   </>
