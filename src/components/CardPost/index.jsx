dc2aebeff15d92972badf749cc16fbf397984411
MatteoTHP <wow-guerrier@hotmail.fr>
2021-06-17 12:23:35 +0200

    completed card post compact and some fixes

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -23,31 +23,31 @@ const CardPost = (article) => {
 
   }, [article])
 
- console.log(article.data.user_id)
-return(
-  <div className="card-post">
-    <BtnLangage />
-    <h3 className="post-title"><a href="/">{article.data.title}</a></h3>
-    <div className="post-author-date">
-      <a href="/" className="post-avatar-container">
-        <img src={avatar} alt="" />
-        <div className="retro-filter"></div>
-      </a>
-      <h4 className="post-username">{User.username} </h4>
-      <span className="date">{article.data.created_at.substring(0, 10)}</span>
+ 
+  return(
+    <div className="card-post">
+      <BtnLangage />
+      <h3 className="post-title"><a href="/">{article.data.title}</a></h3>
+      <div className="post-author-date">
+        <a href="/" className="post-avatar-container">
+          <img src={avatar} alt="" />
+          <div className="retro-filter"></div>
+        </a>
+        <h4 className="post-username">{User.username} </h4>
+        <span className="date">{article.data.created_at.substring(0, 10)}</span>
+      </div>
+      <div className="card-post-img-container">
+        <a href="/">
+          <img src={img_post} alt="Ligne de code sur un écran" />
+          <div className="retro-filter"></div>
+        </a>
+      </div>
+      <div className="card-post-txt-container">
+        <p className="Lead_paragraph">{article.data.lead}</p>
+      </div>
+      <a href={`/articles/${article.data.id}`} className="read_more">Lire la suite</a>
     </div>
-    <div className="card-post-img-container">
-      <a href="/">
-        <img src={img_post} alt="Ligne de code sur un écran" />
-        <div className="retro-filter"></div>
-      </a>
-    </div>
-    <div className="card-post-txt-container">
-      <p className="Lead_paragraph">{article.data.lead}</p>
-    </div>
-    <a href={`/articles/${article.data.id}`} className="read_more">Lire la suite</a>
-  </div>
-  
-)};
+  )
+};
 
 export default CardPost;
