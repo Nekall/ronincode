7152acde545b663d88a4bc5e9fc21c0731335f7d
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-17 17:19:57 +0200

    style: navbar in progress

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,68 @@
 import React from 'react';
 import Logo from "components/Logo";
 // import Sign from "components/Sign";
+import news from 'assets/images/icons/news-regular.svg';
+import calendar from 'assets/images/icons/calendar-alt-regular.svg';
+import message from 'assets/images/icons/comment-regular.svg';
+import profile from 'assets/images/icons/user-regular.svg';
 
-const Navbar = () => (
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { useSelector } from "react-redux";
 
-    <nav className="navbar temp">
+const Navbar = () => {
+
+  const { logged } = useSelector(state => state);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    Cookies.remove('token');
+    Cookies.remove('id');
+    window.location.href = '/signin';
+    return false;
+  };
+
+  return (
+    <nav className="navbar">
       <Logo />
-      <ul>
-        <li><a href="/articles">Articles</a></li>
-        <li><a href="/signup">S'inscrire</a></li>
-        <li><a href="/signin">Se connecter</a></li>
-        <li><a href="/users/sign_out">Se déconnecter</a></li>
-        <li><a href="/users/2">Profil</a></li>
+      <div className="publications">
+        <Link to="/articles">
+          <img src={news} alt="Publications" className="icon" />
+          <h2>Publications</h2>
+        </Link>
+      </div>
+      
+      <ul >
+        {logged ? 
+          <>
+            <li>
+              <Link to="/signup">
+                <img src={calendar} alt="Agenda" className="icon"/>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <img src={message} alt="Messages" className="icon"/>
+              </Link>
+            </li>
+            <li>
+              <Link to="/users/2">
+                <img src={profile} alt="Mon profil" className="icon"/>
+              </Link>
+            </li>
+            <li><Link to="#" >Mentors</Link></li>
+            <li><Link to="#" >Disciples</Link></li>
+            <li><Link to="#" onClick={handleClick}>Se déconnecter</Link></li>
+          </>
+          :
+          <>
+            <li><Link to="/signup">S'inscrire</Link></li>
+            <li><Link to="/signin">Se connecter</Link></li>
+          </>
+        }
       </ul>
-      {/* <Sign /> */}
     </nav>
-);
+  );
+};
 
 export default Navbar;
