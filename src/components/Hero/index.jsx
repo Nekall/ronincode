53bdf58334b85442287f5a02b78f597f28cf9ef5
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-21 00:06:07 +0200

    style: add txt in hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,6 +8,7 @@ const Hero = () => (
     <img src={img_hero} alt="Paysage retrowave 80's" className="hero-background"/>
     <img src={img_hackerman} alt="Paysage retrowave 80's" className="hero-2"/>
     <img src={img_logo} alt="Paysage retrowave 80's" className="hero-3"/>
+    <h2 className="hero-txt-header">Tu veux devenir un grand développeur ?</h2>
     <h2 className="hero-txt">"Ne Code<br/>plus seul !"</h2>
   </div>
 );
