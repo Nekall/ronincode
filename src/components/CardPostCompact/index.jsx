15013f984fd2a5e7fa803a29887ce6b711784fa0
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-23 02:47:48 +0200

    style: restyle homepage

--- src/components/CardPostCompact/index.jsx
+++ src/components/CardPostCompact/index.jsx
@@ -27,6 +27,7 @@ const CardPostCompact = (article) => {
   return(
     <div className="card-postCompact">
       <BtnLangage />
+      <span className="date">{article.data.created_at.substring(0, 10)}</span>
       <div className="card-post-img-container">
         <a href="/">
           <img src={img_post} alt="Ligne de code sur un écran" />
@@ -40,14 +41,15 @@ const CardPostCompact = (article) => {
             <div className="retro-filter"></div>
           </a>
           <h4 className="post-username">{User.username} </h4>
-          <span className="date">{article.data.created_at.substring(0, 10)}</span>
         </div>
         <h3 className="post-title"><a href="/">{article.data.title}</a></h3>
         <div className="card-post-txt-container">
-          <p className="Lead_paragraph">{article.data.lead}</p>
+          <p className="Lead-paragraph">{article.data.lead}</p>
         </div>
       </div>
-      <a href={`/articles/${article.data.id}`} className="read_more">Lire la suite</a>
+      <div className="read-more-container">
+        <a href={`/articles/${article.data.id}`} className="read-more">> Lire la suite</a>
+      </div>
     </div>
   )
 };
