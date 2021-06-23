d8ada354cfb68a271ab78b6b2e511356cf4dc999
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-24 01:27:03 +0200

    style: modif but not used for now

--- src/components/HeroMini/index.jsx
+++ src/components/HeroMini/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import img_hero from 'assets/images/tokyo.png';
-import img_hackerman from 'assets/images/hackerman.png';
-import img_logo from 'assets/images/ronincode-logo-20210617-4.png';
 
 const HeroMini = () => (
-  <div className="heroMini">
+  <div className="hero hero-mini">
     <img src={img_hero} alt="Paysage retrowave 80's" className="hero-background"/>
-    {/* <img src={img_hackerman} alt="Paysage retrowave 80's" className="hero-2"/>
-    <img src={img_logo} alt="Paysage retrowave 80's" className="hero-3"/> */}
-    <div className="hero-txt-container">
-      <div className="hero-txt">
-        <h2>Blog</h2>
-        <h3>Tous les articles de la communauté</h3>
-      </div>
+    <div className="txt-container">
+      <h1>BLOG</h1>
+      <h2>Toutes les publications</h2>
     </div>
   </div>
 );
