fdf7b66e1c1553e75996c61efbad3b51780a4c6f
Neka <nekoanchor@yahoo.fr>
2021-06-24 21:07:15 +0200

    refacto: clean code

--- src/pages/EditArticle/index.jsx
+++ src/pages/EditArticle/index.jsx
@@ -1,50 +1,33 @@
-import React, {useEffect, useState} from 'react'
-import Cookies from 'js-cookie';
-import './style.css'
-import { useHistory } from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import './style.css';
 
+const EditArticle = () => {
 
-
-const EditArticle = () => { 
-
-    
     const token = Cookies.get('token')
     const { articleSlug } = useParams();
     const [technologies, setTechnologies] = useState([])
     const [articleInfo, setarticleInfo] = useState([])
-   
     const history = useHistory()
-
-    
     const [data, setData] = useState({
-      
         title: articleInfo.title,
         lead: articleInfo.lead,
         content: articleInfo.content,
-        technologies: {
-          resource_id: articleSlug,
-          technology_id: articleInfo.technology_id,
-        }
     });
-    
+
     const handleChange = (e) => {
-      const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
       setData({
         ...data,
-        [e.target.name]: value, 
-
+        [e.target.name]: e.target.value,
       })
-      
     };
-
-
-    const url = `https://ronincode.herokuapp.com/resources/${articleSlug}`
 
     const handleFetch = (e) => {
       e.preventDefault();
 
-      fetch(url, {
+      fetch(`https://ronincode.herokuapp.com/resources/${articleSlug}`, {
         method : "PUT",
         headers : {
           "Content-Type" : "application/json",
@@ -55,66 +38,56 @@ const EditArticle = () => {
       .then(data => { setarticleInfo(data)
           history.push(`/blog/${articleSlug}`)
         })
-        
-      } 
+      }
 
 
       const updateFetch = (e) => {
         e.preventDefault();
-        console.log(`data`, data)
+        let techno1 = document.getElementById("techno1").value
+        let joindata = {
+          resource_id: parseInt(articleSlug),
+          technology_id: parseInt(techno1),
+        }
+        console.log(`joindata2`, joindata)
+        console.log(`techno1`, techno1)
         fetch(`https://ronincode.herokuapp.com/resources_technologies/`, {
           method : "POST",
           headers : {
-            'Authorization': `Bearer ${token}`,
+            'Authorization': token,
             "Content-Type" : "application/json",
           },
-          body : JSON.stringify(data)
-         
+          body : JSON.stringify(joindata)
         })
         .then((response) => response.json())
-        .then(data => { setarticleInfo(data)})
-          console.log(`data`, data)
-        } 
+        }
 
         const makeitFetch = (data) => {
           handleFetch(data);
           updateFetch(data);
-          
         }
-    
+
       const technoFetch = () => {
-  
+
         fetch(`https://ronincode.herokuapp.com/technologies/`, {
           method : "GET",
           headers : {
             "Content-Type" : "application/json",
           },
-          
+
         })
         .then((response) => response.json())
         .then((data) => {
           console.log(data)
           setTechnologies(data)
         })
-  
       }
-  
+
         useEffect(() => {
           technoFetch();
       }, [])
 
-      const [checkedState, setCheckedState] = useState(
-        new Array(technologies.length).fill(false)
-      );
-      const handleOnChange = (position) => {
-        const updatedCheckedState = checkedState.map((item, index) =>
-          index === position ? !item : item
-        );
-        setCheckedState(updatedCheckedState);
-        console.log(`object`, updatedCheckedState)
+      console.log(technologies);
 
-      }
-        
     return (
       <div className = "editArticle">
         <h1>Modifier l'Article</h1>
@@ -124,23 +97,19 @@ const EditArticle = () => {
               <input type="text" className="ArticleTitle" name="title" placeholder="Titre de l'article" onFocus={(e) => e.target.placeholder = ''}  onBlur={(e) => e.target.placeholder = "Titre de l'article"} onChange={handleChange}></input>
               <textarea className="ArticleLead" name="lead" placeholder="Introduction" type="textarea" onChange={handleChange}></textarea>
               <textarea className="ArticleContent" name="content" placeholder="Contenu de l'article" type="textarea"  onChange={handleChange}></textarea>
-              <button>Modifier</button>
+              <input type="submit" value="modifier"/>
             </div>
             <div className="Column2">
               <div className="Technologies">
                 <h2>Technologies</h2>
-                <div className="Options">{technologies && technologies.map(({name, id}, index) =>
-                  <div className="Option">
-                    <p key={index}>{name} </p>
-                    <input type="checkbox" id={id}  name="technology_id" value={id} checked={checkedState[index]}  onChange={() => handleOnChange(index)}></input>
-                  </div>
-                  
-                )}
-                  
-                </div>
+                  <select name="techno" id="techno1">
+                    {technologies && technologies.map(({name, id}, index) =>
+                      <option key={Math.random()} value ={id}>{name}</option>
+                    )}
+                  </select>
               </div>
             </div>
-          </div> 
+          </div>
         </form>
       </div>
   );
