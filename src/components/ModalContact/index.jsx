22893b274a8a372e8de00840dd4d95be5c64810b
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-06-24 23:56:52 +0200

    style: layout profile pages in progress

--- src/components/ModalContact/index.jsx
+++ src/components/ModalContact/index.jsx
@@ -11,29 +11,29 @@ const ModalDate = (props) => {
   };
 
   return(
-    <div className="contact">
+    <>
       {props.dataAllUser && props.dataAllUser.map((user) => {
         if (user.id === props.id_user_profile) {
           return(
-            <div key={uuidv4()}>
+            <div key={uuidv4()} className="contact">
               <div className="username">{user.username} ({user.firstname} {user.lastname})</div>
               {props.logged && (props.id_current !== props.id_user_profile) ? 
-                <div>
-                  <Link to="/rendez-vous" className="btn-message">Prendre RDV</Link>
+                <div className="buttons">
+                  <Link to="/rendez-vous" className="btn-message">Prendre rendez-vous</Link>
                   {ifClicked ? <button onClick={clicked} className="btn-message">X</button> : <button onClick={clicked} className="btn-message">Envoyer un message à {user.email}</button>}
                   {ifClicked ? <ModalCreateConversation id={props.id_user_profile} /> : ""}
                 </div>
                 :
-                <div>
-                  <Link to="/conversations" className="btn-message">Mes Conversations</Link>
-                  <Link to="/profile-edit" className="btn-message">Edit Profile</Link>
+                <div className="buttons">
+                  {/* <Link to="/conversations" className="btn-message">Messages</Link> */}
+                  <Link to="/profile-edit" className="btn-edit">Éditer le profil</Link>
                 </div>
               }
             </div>
           );
         }
       })}
-    </div>
+    </>
   );
 };
 
