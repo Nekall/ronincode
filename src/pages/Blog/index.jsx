264ef721ebf0187cd830b2e58a937ebcdf62bc0b
ZugastiKevin <rochambeaut@gmail.com>
2021-06-25 16:19:48 +0200

    feat: add blog link to create article

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
 import CardGroupPostCompact from "components/CardGroupPostCompact/index.jsx";
 
 const Blog = () => {
+  const logged = useSelector(state => state.logReducer.logged);
   return(
     <>
       {/* <HeroMini /> */}
+      {logged ? <Link to="/nouvel-article">Écrire un article</Link> : ""}
       <CardGroupPostCompact />
     </>
   )
