479154e117924d7e4885bfed5a8f10338f92fa01
Neka <nekoanchor@yahoo.fr>
2021-06-27 15:26:48 +0200

    feat: add responsive for statics pages

--- src/pages/LegalMention/index.jsx
+++ src/pages/LegalMention/index.jsx
@@ -8,7 +8,7 @@ const LegalMention = () => {
     <ol>
       <br/>
       Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site : <a href="#" target="_blank">www.ronincode.vercel.app</a> les informations suivantes :
-      <li className="bold">1. Informations légales :</li>
+      <li className="bold">Informations légales :</li>
       Statut du propriétaire : particulier<br/>
       Le Propriétaire est : Prenom Nom<br/>
       Adresse postale du propriétaire : Adresse 66666 Ville<br/>
@@ -22,28 +22,28 @@ const LegalMention = () => {
       Contacter le Webmaster :  <a href="mailto:email@webmaster.fr?subject=Contact a partir des mentions lÃ©gales via le site www.ronincode.vercel.app">email@webmaster.fr</a><br/>
       L’hebergeur du site est : Nom de l'hébergeur du site Adresse de l'hébergeur du site Code postal de l'hebergeur Ville de l'hebergeur
       <br/><br/>
-      <li className="bold">2. Présentation et principe :</li>
+      <li className="bold">Présentation et principe :</li>
       <br/><br/>
       Est désigné ci-après : Utilisateur, tout internaute se connectant et utilisant le site susnommé : <a href="#" target="_blank">www.ronincode.vercel.app</a>.<br/>
       Le site www.ronincode.vercel.app regroupe un ensemble de services, dans l'état,  mis à la disposition des utilisateurs. Il est ici précisé que ces derniers doivent rester courtois et faire preuve de bonne foi tant envers les autres utilisateurs qu'envers le webmaster du site www.ronincode.vercel.app. Le site www.ronincode.vercel.app est mis à jour régulièrement par Le webmaster.<br/>
       Prenom Nom s’efforce de fournir sur le site www.ronincode.vercel.app des informations les plus précises possibles (sous réserve de modifications apportées depuis leur mise en ligne), mais ne saurait garantir l'exactitude, la complétude et l'actualité des informations diffusées sur son site, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. En conséquence, l'utilisateur reconnaît utiliser ces informations données (à titre indicatif, non exhaustives et susceptibles d'évoluer) sous sa responsabilité exclusive.
       <br/><br/>
-      <li className="bold">3. Accessibilité :<br/></li>
+      <li className="bold">Accessibilité :<br/></li>
       <br/><br/>
       Le site www.ronincode.vercel.app est par principe accessible aux utilisateurs 24-24h, 7-7j, sauf interruption, programmée ou non, pour les besoins de sa maintenance ou en cas de force majeure. En cas d’impossibilité d’accès au service, www.ronincode.vercel.app s’engage à faire son maximum afin de rétablir l’accès au service et s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.  N’étant soumis qu’à une obligation de moyen, www.ronincode.vercel.app ne saurait être tenu pour responsable de tout dommage, quelle qu’en soit la nature, résultant d’une indisponibilité du service.
       <br/><br/>
-      <li className="bold">4. Propriété intellectuelle :</li>
+      <li className="bold">Propriété intellectuelle :</li>
       <br/><br/>
       Prenom Nom est propriétaire exclusif de tous les droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, tant sur la structure que sur les textes, images, graphismes, logo, icônes, sons, logiciels…<br/>
       Toute reproduction totale ou partielle du site www.ronincode.vercel.app, représentation, modification, publication, adaptation totale ou partielle de l'un quelconque de ces éléments, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Prenom Nom, propriétaire du site à l'email : email@webmaster.fr, à défaut elle sera considérée comme constitutive d’une contrefaçon et passible de poursuite conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
       <br/><br/>
-      <li className="bold">5. Liens hypertextes et cookies :</li>
+      <li className="bold">Liens hypertextes et cookies :</li>
       <br/><br/>
       Le site www.ronincode.vercel.app contient un certain nombre de liens hypertextes vers d’autres sites (partenaires, informations …) mis en place avec l’autorisation de Prenom Nom. Cependant, Prenom Nom n’a pas la possibilité de vérifier l'ensemble du contenu des sites ainsi visités et décline donc toute responsabilité de ce fait quand aux risques éventuels de contenus illicites.<br/>
       L’utilisateur est informé que lors de ses visites sur le site www.ronincode.vercel.app, un ou des cookies sont susceptibles de s’installer automatiquement sur son ordinateur par l'intermédiaire de son logiciel de navigation. Un cookie est un bloc de données qui ne permet pas d'identifier l'utilisateur, mais qui enregistre des informations relatives à la navigation de celui-ci sur le site. <br/>
       Le paramétrage du logiciel de navigation permet d’informer de la présence de cookie et éventuellement, de la refuser de la manière décrite à l’adresse suivante : <a href="http://www.cnil.fr">www.cnil.fr</a>. L’utilisateur peut toutefois configurer le navigateur de son ordinateur pour refuser l’installation des cookies, sachant que le refus d'installation d'un cookie peut entraîner l’impossibilité d’accéder à certains services. Pour tout bloquage des cookies, tapez dans votre moteur de recherche : bloquage des cookies sous IE ou firefox et suivez les instructions en fonction de votre version.
       <br/><br/>
-      <li className="bold">6. Protection des biens et des personnes - gestion des données personnelles :</li>
+      <li className="bold">Protection des biens et des personnes - gestion des données personnelles :</li>
       <br/><br/>
       En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.
       Sur le site www.ronincode.vercel.app, Prenom Nom ne collecte des informations personnelles ( suivant l'article 4 loi n°78-17 du 06 janvier 1978) relatives à l'utilisateur que pour le besoin de certains services proposés par le site www.ronincode.vercel.app. L'utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu'il procède par lui-même à leur saisie. Il est alors précisé à l'utilisateur du site www.ronincode.vercel.app l’obligation ou non de fournir ces informations.<br/>
